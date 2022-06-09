aaa9152ec57d2409e8224ef0356078b54277dbb2
Arpit Bansal <84992238+arpitkb@users.noreply.github.com>
2022-06-09 12:54:05 +0530

    Update WorkFlow.java

--- src/main/java/com/github/arpitkb/kafka/WorkFlow.java
+++ src/main/java/com/github/arpitkb/kafka/WorkFlow.java
@@ -1,20 +1,41 @@
 package com.github.arpitkb.kafka;
 
+import java.util.ArrayList;
+
 public class WorkFlow {
     private String id;
+
+    private String parent_id;
     private String name;
     private String status;
 
-//    public WorkFlow() {}
+    private ArrayList<WorkFlow> nodes;
+
+   public WorkFlow() {}
 
     public WorkFlow(String id, String name, String status) {
+        this.parent_id = null;
         this.id = id;
         this.name = name;
         this.status = status;
+        nodes=new ArrayList<>();
+    }
+
+    public void addChild(WorkFlow a){
+        a.parent_id = id;
+        nodes.add(a);
     }
 
     public String getId() {
         return id;
+    }
+
+    public ArrayList<WorkFlow> getNodes(){
+        return nodes;
+    }
+
+    public String getParentId() {
+        return parent_id;
     }
 
     public String getName() {
@@ -25,15 +46,15 @@ public class WorkFlow {
         return status;
     }
 
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public void setStatus(String status) {
-//        this.status = status;
-//    }
+   public void setId(String id) {
+       this.id = id;
+   }
+
+   public void setName(String name) {
+       this.name = name;
+   }
+
+   public void setStatus(String status) {
+       this.status = status;
+   }
 }
